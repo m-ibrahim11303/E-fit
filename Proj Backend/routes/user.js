ba5ca90227af4a5b6c6d98c77e6b9165bfaa0c47
Hussain-SZ <hussain.sulaiman3@yahoo.com>
2025-04-09 20:42:37 +0500

    Meal Logging Corrected

--- Proj Backend/routes/user.js
+++ Proj Backend/routes/user.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createUser, loginUser, saveMeals } from "../controllers/user.js";
+import { createUser, loginUser, saveMeals , getDietHistory} from "../controllers/user.js";
 
 export const userRouter = express.Router();
 
 userRouter.post("/create", createUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/savemeals", saveMeals)
+userRouter.get("/diethistory", getDietHistory)
+
 // localhost:8000/user/get
