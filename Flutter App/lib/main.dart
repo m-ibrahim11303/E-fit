1f7d5b731f9302d3ff8ba71fc811ef9810d2939b
Huzaifa20032 <62719237+Huzaifa20032@users.noreply.github.com>
2025-04-25 22:21:08 +0500

    Update main.dart

--- Flutter App/lib/main.dart
+++ Flutter App/lib/main.dart
@@ -3,22 +3,18 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart'; // Add secu
 import 'bootup/bootup_page_1.dart'; // Import the BootupPage1 widget
 import 'home_page.dart'; // Import the HomeScreen widget
 
-// Global secure storage instance
 final FlutterSecureStorage storage = FlutterSecureStorage();
 
 void main() async {
-  // Ensure that Flutter bindings are initialized for async operations
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Check if email exists in secure storage
   final String? email = await storage.read(key: 'email');
 
-  // Run the app with conditional initial route
   runApp(MyApp(initialRoute: email != null));
 }
 
 class MyApp extends StatelessWidget {
-  final bool initialRoute; // True if email exists, false otherwise
+  final bool initialRoute;
 
   const MyApp({Key? key, required this.initialRoute}) : super(key: key);
 
